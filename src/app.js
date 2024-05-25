062d3c2680e37f771e116445dfb349444c555ba7
Naman Jindal <namanjindal113@gmail.com>
2024-05-25 21:41:18 +0530

    done 2

--- src/app.js
+++ src/app.js
@@ -22,6 +22,33 @@ app.get('/tours', (req, res) => {
 
 app.post('/tours', (req, res) => {
   const { name, description, duration, price } = req.body;
+  /* 
+    Be -> DB -> FE -> 100 fields -> 4 
+  
+  */
+ //read the file
+ //append in the data 
+ //write the file 
+ fs.readFile(`${__dirname}/data/tours.json`, (err, data) => {
+    if(err){
+      console.log(err);
+      res.status(500).json({message: 'Internal Server Error'});
+      return;
+    }
+    const tourData = JSON.parse(data);
+    const newId = tourData[tourData.length-1].id + 1;
+    const newTour = {name, description, duration, price , newId}
+    tourData.push(newTour)
+
+    fs.writeFile(`${__dirname}/data/tours.json`, JSON.stringify(tourData), (err)=>{
+      if(err){
+        console.log(err);
+        res.status(500).json({message: 'Internal Server Error'});
+        return;
+      }
+      res.status(200).json({ message: 'Tour added successfully'})
+    })
+  })
   //Write a code here for creating new tour from data/tours.json
   //For creating new id use this logic
   // const newId = tourDetails[tourDetails.length - 1].id + 1;
